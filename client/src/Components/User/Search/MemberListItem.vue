36bfa7f7693ae30f69a06f722f5e41d008cdde5a
Gaston ALAMACHERE <gaston.alamachere@gmail.com>
2018-06-19 18:09:51 +0200

    changements dans search

---
+++ client/src/Components/User/Search/MemberListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-container class="bv-example-row">
+      <b-row class="text-center">
+          <b-col>{{ member.photo }}</b-col>
+          <b-col cols="6">{{ member.text }}</b-col>
+          <b-col cols="2">{{ member.likes }}
+            <button @click="member.likes++">+moi
+            </button></b-col>
+      </b-row>
+  </b-container>
+</template>
+
+<script>
+  export default {
+  props: {
+    member: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- client/src/Components/User/Search/SearchContent.vue
+++ client/src/Components/User/Search/SearchContent.vue
@@ -1,3 +1,50 @@
+// affichage liste de membres
+
 <template>
-  <div>Search Content</div>
+  <div>
+    <ul v-if="members.length">
+      <MemberListItem
+        v-for="member in members"
+        :key="member.id"
+        :member="member"/>
+    </ul>
+    <p v-else>
+      Aucun membre dans votre sélection.
+    </p>
+  </div>
 </template>
+
+<script>
+import MemberListItem from './MemberListItem.vue'
+let nextMemberId = 0
+
+export default {
+  components: {
+    MemberListItem
+  },
+  data () {
+    return {
+      members: [
+        {
+          id: nextMemberId++,
+          photo: 'Photo Étoile',
+          text: 'Étoile, 60 ans, Designer, Paris ',
+          likes: 2
+        },
+        {
+          id: nextMemberId++,
+          photo: 'Photo MarieLaure',
+          text: 'MarieLaure, 57 ans, Hospitalière, Paris ',
+          likes: 3
+        },
+        {
+          id: nextMemberId++,
+          photo: 'Photo Nina',
+          text: 'Nina, 57 ans, Artiste, Paris ',
+          likes: 1
+        }
+      ],
+    }
+  }
+}
+</script>
