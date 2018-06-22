7277569cf20622922d96a44c6791cdf79bc6b78d
Pascal VENTURINI <pventuri@student.42.fr>
2018-06-22 11:02:25 +0200

    Fichiers

--- server/models/gallery.js
+++ server/models/gallery.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 
-const UserSchema = new mongoose.Schema({
+const GallerySchema = new mongoose.Schema({
     url: {
       type: String,
       index: {unique: true}
     }
 })
+
+
+module.exports = mongoose.model('Image', GallerySchema)
--- server/models/hobbie.js
+++ server/models/hobbie.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 
-const UserSchema = new mongoose.Schema({
+const HobbiSchema = new mongoose.Schema({
     name: {
       type: String,
       index: {unique: true}
     }
 })
+ module.exports = mongoose.model('Image', HobbiSchema)
