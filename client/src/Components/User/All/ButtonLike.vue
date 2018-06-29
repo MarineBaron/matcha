15356ff3538d4ce8ff0f4927b9b5769439b3d8d0
Marine BARON <mbaron@student.42.fr>
2018-06-29 09:43:03 +0200

    User / Actions : add Component BuutonLike in User/All

---
+++ client/src/Components/User/All/ButtonLike.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-button
+    @click.prevent="onClick"
+  >{{value}}</b-button>
+</template>
+
+<script>
+  export default {
+    props: ['type', 'username'],
+    methods: {
+      onClick() {
+        console.log("onClick: ", this.type, this.username)
+      }
+    },
+    computed: {
+      value() {
+        switch('type') {
+          case 'like':
+          case 'relike':
+            return 'Like'
+          break
+          case 'unlike'
+            return 'Unlike'
+          break
+          default:
+            return 'No action'
+          break
+        }
+      }
+    }
+  }
+</script>
