3126b77022f5f5a5e1004311af0a1e62bede89eb
Pascal VENTURINI <pventuri@student.42.fr>
2018-08-19 11:54:48 +0200

    retour vacs

--- client/src/Components/Home/HomeAnonymous.vue
+++ client/src/Components/Home/HomeAnonymous.vue
@@ -1,10 +1,9 @@
 <template>
   <b-container fluid>
-    <h2>Accueil</h2>
         <b-row>
       <b-col md="8">
         <br/>
-        <h3> Bienvenue chez... Vous ! ;)</h3>
+        <h3> Bienvenue ! ;)</h3>
         <p style="text-align:justify;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum tempor tortor, ut aliquam lorem condimentum sed. Pellentesque ut est ex. Maecenas consectetur in metus vel pretium. Suspendisse molestie orci quis nisl ornare egestas. Quisque eu ipsum sit amet sem rhoncus rutrum. Aenean ac finibus augue. Sed tempor facilisis orci eu vehicula. In dignissim sapien vitae ex gravida, et mollis dui maximus. Integer vitae quam porttitor orci eleifend dapibus in ac diam. Etiam sagittis odio felis, ultrices porta dui viverra a. Integer ut gravida ipsum. Sed laoreet tempus risus, in egestas arcu efficitur et. Aenean bibendum vulputate mi, eget sodales urna gravida ut. Nam augue odio, dictum quis ipsum non, suscipit feugiat nunc. Duis blandit viverra purus, tincidunt imperdiet lacus tincidunt nec. Duis non consequat diam.
         <br />
         Vivamus tincidunt, arcu sed convallis fringilla, ante nulla elementum lorem, ac posuere est erat at dolor. Aliquam tempus ipsum nulla, et facilisis mi accumsan ullamcorper. Etiam pellentesque, ante sed luctus consequat, orci nulla viverra urna, vitae imperdiet nunc justo convallis metus. Sed et eleifend tortor. Fusce a scelerisque ipsum, sed malesuada augue. Fusce a justo condimentum, mattis sapien ac, congue neque. Vestibulum nec erat nisl. Aliquam commodo rutrum dolor, non volutpat nulla volutpat nec. Morbi elit magna, dapibus at elementum et, auctor et nisi.
--- client/src/Components/Home/HomeAuthenticated.vue
+++ client/src/Components/Home/HomeAuthenticated.vue
@@ -1,8 +1,10 @@
 <template>
   <b-container fluid>
-    <h2>Accueil</h2>
     <b-row>
-      <b-col md="8">
+      <b-col md="8"><h3> Bienvenue chez... Vous ! ;)</h3>
+        <p style="text-align:justify;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi interdum tempor tortor, ut aliquam lorem condimentum sed. Pellentesque ut est ex. Maecenas consectetur in metus vel pretium. Suspendisse molestie orci quis nisl ornare egestas. Quisque eu ipsum sit amet sem rhoncus rutrum. Aenean ac finibus augue. Sed tempor facilisis orci eu vehicula. In dignissim sapien vitae ex gravida, et mollis dui maximus. Integer vitae quam porttitor orci eleifend dapibus in ac diam. Etiam sagittis odio felis, ultrices porta dui viverra a. Integer ut gravida ipsum. Sed laoreet tempus risus, in egestas arcu efficitur et. Aenean bibendum vulputate mi, eget sodales urna gravida ut. Nam augue odio, dictum quis ipsum non, suscipit feugiat nunc. Duis blandit viverra purus, tincidunt imperdiet lacus tincidunt nec. Duis non consequat diam.
+        </p>
+        <h2>Carte de vos relations</h2>
         <map-user-all
         type="home"
         status="success"
